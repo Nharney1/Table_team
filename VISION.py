932d909fdad66f7ce86a17bfff8932082b6f5d35
Noah <nharney1@knights.ucf.edu>
2023-01-20 23:02:56 -0500

    Add ball class and clean up repo

--- VISION.py
+++ VISION.py
@@ -2,10 +2,27 @@ from src.HoughCircle import DetectCircles
 from src.AnkerCameraLibrary import AnkerCamera
 from src.BluetoothServer import BluetoothServerSocket
 
+
 def main():
-	DetectCircles()
-	# myCam = AnkerCamera(1) # Leave this commented out when the camera is not connected
-	pi_bluetooth_socket = BluetoothServerSocket(10)
+
+	# Initialize game variables
+	Startup = True
+	Current_Ball_List = None
+	Previous_Ball_List = None
+
+	# Initialize connections
+	myCam = AnkerCamera(2) # 1 on Jetson Nano; 2 on laptop
+	pi_bluetooth_socket = BluetoothServerSocket(10) # Port 10 (arbitrary choice)
+	myCam.take_video()
+
+	while(True):
+		Startup = False
+
+		# Begin computer vision
+		myCam = AnkerCamera(2)
+		myCam.take_picture() # 1 on Jetson Nano; 2 on laptop
+		Current_Ball_List = DetectCircles()
+		break
 
 
 
--- src/HoughCircle.py
+++ src/HoughCircle.py
@@ -1,17 +1,21 @@
 import sys
 import cv2 as cv 
 import numpy as np
+from .Ball import Ball
 
 # Define the minimum and maximum coordinates for the billiard balls
 X_MIN = 60
-X_MAX = 1160
-Y_MIN = 70
-Y_MAX = 610
+X_MAX = 1150
+Y_MIN = 25
+Y_MAX = 570
+ERROR = 10
+
+SHOW_IMAGES = True
 
 def DetectCircles():
-	# Read in the input image
-	# path = '/home/table_team/Desktop/testing/test_images/input_02.png' # This will need to change, current image is a sample image
-	path = 'C:/Users/UHSgi/Desktop/SD_scripts/cv/input_09.png'
+	Ball_list = []
+
+	path = 'C:/Users/UHSgi/Desktop/SD_scripts/cv/input_26.png' # This will need to change, current image is a sample image
 	img = cv.imread(path, cv.IMREAD_COLOR)
 
 	# Resize the image, needed for displaying the output image
@@ -20,70 +24,50 @@ def DetectCircles():
 	dim = (width, height)
 	resize_img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
-	cv.imshow('balls', resize_img) # Testing only
-	cv.waitKey(0)
-	# Defined as:			y 		x
-	resize_img = resize_img[50:720, 0:1200]
-	cv.circle(resize_img, (60,70), 5, (255,0,0,), 5)
-	cv.circle(resize_img, (1160,610), 5, (255,0,0,), 5)
-	cv.imshow('balls', resize_img) # Testing only
-	cv.waitKey(0)
+	# Crop the image [y:x]
+	resize_img = resize_img[60:680, 40:1240]
+
+	if SHOW_IMAGES:
+		# Show the minimum and maximum location for balls (corners of table)
+		cv.circle(resize_img, (X_MIN, Y_MIN), 5, (255,0,0,), 5)
+		cv.circle(resize_img, (X_MAX,Y_MAX), 5, (255,0,0,), 5) 
+		cv.imshow('balls', resize_img) 
+		cv.waitKey(0) 
 
 	# Convert to grayscale for the Hough Circle Transform
 	gray_img = cv.cvtColor(resize_img, cv.COLOR_BGR2GRAY)
-	rows = gray_img.shape[0]
 
-	# param1 sets sensitivity (how strong edges need to be). Higher values only pick up detailed edges
-	# param2 sets how many edge points are needed. Lower finds more circle
-	# param1, param2 = 36,22 seems to be the best values for accuracy
-	circles = cv.HoughCircles(gray_img, cv.HOUGH_GRADIENT, 1, rows/8, param1 = 36, param2 = 21, minRadius = 15, maxRadius = 25)
+	# Minimum distance between balls is 15 pixels
+	min_distance = 15
+	
+	# Make the call to detect circles in image
+	circles = cv.HoughCircles(gray_img, cv.HOUGH_GRADIENT, 1, min_distance, param1 = 30, param2 = 14, minRadius = 15, maxRadius = 25)
 
 	if circles is not None:
 		circles = np.uint16(np.around(circles))
 		for i in circles[0,:]:
-			#			x     y
-			center = (i[0], i[1])
+
+			# Extract information from contours
 			x = i[0]
 			y = i[1]
+			center = (i[0], i[1])
 			radius = i[2]
+
+			# Skip over any noise in the corners
+			if x < X_MIN + ERROR or x > X_MAX - ERROR or y < Y_MIN + ERROR or y > Y_MAX - ERROR:
+				continue
+
+			cv.circle(resize_img, center, radius, (255,0,255), 3) # Draw a circle around the ball
+
+			# Determine the color of the ball
 			temp_ball = Ball(x,y,radius)
 			temp_ball.CleanBGRVector(resize_img)
-			arr = resize_img[y, x]
-			if x < X_MIN or x > X_MAX or y < Y_MIN or y > Y_MAX:
-				continue
-			cv.circle(resize_img, center, radius, (255,0,255), 3)
-			# print(radius)
-			# print(center)
-			# print(arr)
-			print('\n')
-			cv.imshow('balls', resize_img)
-			cv.waitKey(0)
+			temp_ball.SetColor()
+			Ball_list.append(temp_ball)
 
-
-# Need to define the thresholds for color here after experimented with them
-
-
-class Ball(object):
-	def __init__(self, x, y, radius):
-		self.x = x
-		self.y = y
-		self.radius = radius
-		self.bgr_list = None
-		self.color = None
-
-	def CleanBGRVector(self, img):
-		intervals = [-8, -6, -4, -2, 0, 2, 4, 6, 8]
-		blue = 0
-		green = 6
-		red = 0
-		for i in range (len(intervals)):
-			for j in range (len(intervals)):
-				# print("{} , {}".format(intervals[i],intervals[j]))
-				print(img[self.y + intervals[j], self.x + intervals[i]])
-				blue += img[self.y + intervals[j], self.x + intervals[i]][0]
-				green += img[self.y + intervals[j], self.x + intervals[i]][1]
-				red += img[self.y + intervals[j], self.x + intervals[i]][2]
-		print("{}, {}, {}".format(int(blue/81), int(green/81), int(red/81)))
+			if SHOW_IMAGES: 
+				cv.imshow('balls', resize_img)
+		return Ball_list
 
 
 if __name__ == "__main__":
