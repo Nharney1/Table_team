30027c539e440e6848c7b737cb9911e4c8b2945f
Noah <nharney1@knights.ucf.edu>
2023-03-27 23:17:24 -0400

    AI: Improve AI location conversion to include every half speaker distance

--- VISION.py
+++ VISION.py
@@ -5,8 +5,7 @@ from src.BluetoothServer import BluetoothServerSocket
 from src.GameState import InvalidBallCount, DetectShot
 from src.BLE_Testing import Init_BLE
 from src.ShotSelectionHelper import computeShot
-from src.Speakers import DetermineNextSpeaker
-from src.ShotSelectionToSpeaker import ConvertSSToSpeaker
+from src.Speakers import DetermineNextSpeaker, ConvertSSToSpeaker
 
 from src import Settings
 from src.MQTT_Localization import MQTT_Main
@@ -19,44 +18,60 @@ import paho.mqtt.client as mqtt
 def main():
 
 	# Initialize game variables
-	Startup = True
 	Continue = False
 	Current_Ball_List = None
 	Previous_Ball_List = None
-	temp_current_position = None
+	Current_Position = None
+	Target_Speakers = []
+	Current_Speakers = []
+	Guidance_Speakers = []
 	
 
 	#Initialize connections
 	Settings.InitializeGlobals()
 	#Init_BLE()
-	myCam = AnkerCamera(0) # 1 on Jetson Nano; 2 on laptop
-	#pi_bluetooth_socket = BluetoothServerSocket(10) # Port 10 (arbitrary choice)
-
 	#mqttc = mqtt.Client(transport='websockets')   
 	#mqttc.connect('broker.emqx.io', 8083, 60)
 	#mqttc.subscribe("t/sd/feedback", 0)
 	#MQTT_Thread = threading.Thread(target = MQTT_Main)
 	#MQTT_Thread.start()
-	#pi_bluetooth_socket.initServerSocket()
-	myCam.take_video()
-	cv2.destroyAllWindows()
-	print("Initialization Complete!")
-	time.sleep(2)
+	#myCam = AnkerCamera(0) # 1 on Jetson Nano; 2 on laptop
+	#myCam.take_video()
+	#cv2.destroyAllWindows()
+	#print("Initialization Complete!")
+	#time.sleep(2)
 
 	while True:
-		#print("sleeping")
-		#time.sleep(1)
-		#val = int(input("Enter number: "))
-		#Settings.noah_char.write_value(val.to_bytes(1,byteorder='big', signed=False))
-		#time.sleep(2)
-		#for i in range(10):
-			#myCam.take_picture()
+		# for i in range(3):
+		# 	myCam.take_picture()
 
 		Current_Ball_List = DetectCircles()
 		computedShot : ComputedShot = computeShot(Current_Ball_List=Current_Ball_List)
-		target_x , target_y = computedShot.playerPos
-		print(str(target_x) + ' ' + str(target_y))
-		print("Desired speaker is " + str(ConvertSSToSpeaker(target_x, target_y)))		
+		Target_Speakers = ConvertSSToSpeaker(computedShot.playerPos[0], computedShot.playerPos[1])
+		Target_Speakers.sort()
+
+		while True:
+			# Get current position represented as speakers
+			Settings.MQTT_Lock.acquire()
+			Current_Location = Settings.MQTT_Location
+			print(Current_Location)
+			Settings.MQTT_Lock.release()
+
+			Current_Speakers = ConvertULToSpeaker(Current_Location[0], Current_Location[1]) #TODO
+			Current_Speakers.sort()
+
+			if Target_Speakers == Current_Speakers:
+				# Is there a better way to do this?
+				time.sleep(5)
+				if Target_Speakers == Current_Speakers:
+					break
+			else:
+				# We are not at the desired speaker(s) yet
+				Guidance_Speakers = DetermineNextSpeaker(Current_Speakers, Target_Speakers)
+
+
+
+
 		#if Previous_Ball_List is not None:
 			#try:
 				#ret = DetectShot(Previous_Ball_List, Current_Ball_List)
@@ -66,33 +81,23 @@ def main():
 			#except InvalidBallCount:
 				#print("Invalid ball count, something is wrong")
 			#break
-		# In here will be the user localization and guidance
-
-		#print("VISION sleeping")
-		#time.sleep(1)
-
-		#Settings.MQTT_Lock.acquire()
-		#temp_current_position = Settings.MQTT_Location
-		#print(temp_current_position)
-		#Settings.MQTT_Lock.release()
-		
-		# Below is how to write a byte to the ESP32
-		# Settings.noah_char.write_value(val.to_bytes(1,byteorder='big', signed=False))
 
 		# Current shot is complete, store current game state
 		#Previous_Ball_List = Current_Ball_List
-
-		#Startup = False
 
 		#shot_result_feedback = 0
 		#ret= mqttc.publish("t/sd/feedback",shot_result_feedback)   
 		#for i in range(20):
 			#print('Sleeping')
 			#time.sleep(1)
+
 		#cv2.destroyAllWindows()
 
 # CLEANUP myCam.shutdown()
 
+
+# Below is how to write a byte to the ESP32
+# Settings.noah_char.write_value(val.to_bytes(1,byteorder='big', signed=False))
 
 if __name__ == "__main__":
 	main()
